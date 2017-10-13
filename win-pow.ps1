9198dfb43380117b34bf48cc42cb55ee3c276b8b
kazimodemis <32776885+kazimodemis@users.noreply.github.com>
2017-10-13 21:57:13 +0300

    Update win-pow.ps1

--- win-pow.ps1
+++ win-pow.ps1
@@ -4,19 +4,23 @@ arp -a
 Write-Output " --------------------------------------------------"
 
 ###Routing Tablosunu Görmemizi Sağlıyor###
+Write-Output "Routing Table:"
 netstat -r
 Write-Output " --------------------------------------------------"
 
 ###CPU Bilgilerini Veriyor###
+Write-Output "CPU:"
 gwmi Win32_processor | Select Name,ProcessId
 Write-Output " --------------------------------------------------"
 
 
 ###Kullanıcıların Bilgilerini Veriyor###
+Write-Output "USERS:"
 gwmi -computer $computer_name Win32_UserAccount | foreach {$_.Name}
 Write-Output " --------------------------------------------------"
 
 ###Sistem Hakkında Bilgiler###
+Write-Output "Sistem Bilgi:"
 systeminfo | findstr "OS" | findstr "Name:"
 systeminfo | findstr "OS"| findstr "Version:" 
 systeminfo | findstr "OS"| findstr "Build Type:" 
@@ -31,10 +35,12 @@ schtasks
 Write-Output " --------------------------------------------------"
 
 ###Domain Bilgileri###
+Write-Output "Domain:"
 Get-ADDomain 
 Write-Output " --------------------------------------------------"
     
 ###Grup Bilgileri###
+Write-Output "Grup:"
 Get-ADGroup
 Write-Output " --------------------------------------------------"
  
@@ -43,10 +49,10 @@ Get-ADDomainController
 Write-Output " --------------------------------------------------"
 
  ###Komut Geçmişini Gösterir###
-Get-History 
-Write-Output " --------------------------------------------------"
+ Write-Output "Komut Geçmişi:"
+ Get-History 
+ Write-Output " --------------------------------------------------"
 
 ###Çalışan Servisler###
 get-service | where-object {$_.Status -eq "Running"} | Select-Object Name
 Write-Output " --------------------------------------------------"
-
